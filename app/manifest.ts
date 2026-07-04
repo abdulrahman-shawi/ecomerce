5a13c9981472b0e24c76f0359749c3effd3f3aa7
abdulrahman-shawi <aboodi.sy91@gmail.com>
2026-07-04 21:52:41 +0300

    fix: correct purpose attribute for icons in manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -23,13 +23,13 @@ export default async function manifest(): Promise<MetadataRoute.Manifest> {
         src: "/icon?size=192",
         sizes: "192x192",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "maskable",
       },
       {
         src: "/icon?size=512",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "maskable",
       },
       {
         src: "/apple-icon",
